d2d04fffbacbf0ac8aefd13cc88c1c6d2cfb3dc4
Kisakye Dhisayar <92805452+Kisakye-Desire@users.noreply.github.com>
2022-10-27 06:08:24 -0700

    Add files via upload

---
+++ tut.py
@@ -0,0 +1,13 @@
+
+
+#Classes help you form your own datatypes e.g Jim and Joan datatype.
+class Student:
+    pass
+
+#Jim is an instance of the student class.
+jim =Student ()
+print(jim)
+
+joan ="Garry"
+print(joan)
+
