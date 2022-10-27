0432f5ca41fda619e610112a7d0a021792be314e
Kisakye Dhisayar <92805452+Kisakye-Desire@users.noreply.github.com>
2022-10-27 06:07:57 -0700

    Add files via upload

---
+++ f_string.py
@@ -0,0 +1,14 @@
+
+#F string enables us to join strings 
+name="Desire"
+
+age=32
+
+Gender="Female"
+
+
+#Without use of f string
+#print("My name is"+name+"And my age is "+ str(age) +"and Iam "+Gender)
+
+#Use of the f string
+print(f"My name is  {name} and I am  {age} and my gender is  {Gender}")
